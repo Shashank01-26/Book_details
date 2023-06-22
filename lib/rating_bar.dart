79cf8c3cef2705326266a5ce0925d1eef8ca79fa
Shashank Kumar Srivastava <srivastavashashank033@gmail.com>
2023-06-23 02:31:02 +0530

    Added ratings

---
+++ lib/rating_bar.dart
@@ -0,0 +1,42 @@
+// import 'package:flutter/widgets.dart';
+
+// class RatingBar extends StatelessWidget {
+//   final double rating;
+
+//   const RatingBar(this.rating, {Key? key}):super(key:key);
+//   @override
+//   Widget build(BuildContext context) {
+//     return Row(
+//       mainAxisSize: MainAxisSize.min,
+//       children: List.generate(rating.floor(),(index)){
+//         return Icon(
+//           Icons.star,
+//           color:Colors.gold,
+//           size:16,
+//         );
+//       }
+//     );
+//   }
+// }
+import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+
+class RatingBar extends StatelessWidget {
+  final double rating;
+
+  const RatingBar({required this.rating,super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: List.generate(rating.floor(), (index) {
+        return Icon(
+          Icons.star,
+          color: Colors.amberAccent,
+          size: 16,
+        );
+      }),
+    );
+  }
+}
